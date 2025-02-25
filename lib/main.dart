cf5fc3bc49e947e50a1cf79d780da3c5a26b20a3
John Gomez <johngomez45@Johns-MacBook-Pro.local>
2025-02-24 20:42:46 -0500

    added/changed the app bar and background colors

--- lib/main.dart
+++ lib/main.dart
@@ -30,3 +30,34 @@ class Counter with ChangeNotifier {
   }
 }
 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Counter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Age Counter'),
+        backgroundColor: Colors.white, // Set app bar to light blue
+      ),
+      body: Consumer<Counter>(
+        builder: (context, counter, child) {
+          String message;
+          Color backgroundColor = Colors.white; // Default background to white
